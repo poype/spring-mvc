8c1d61584e020044de2f0e89470fa980ef5ebe95
Poype <253186926@qq.com>
2021-08-08 14:23:42 +0800

    两个接口的 URI 都是 /test_method，根据其 method 类型做路由

--- src/main/java/com/poype/springmvc/HelloWorld.java
+++ src/main/java/com/poype/springmvc/HelloWorld.java
@@ -1,7 +1,9 @@
 package com.poype.springmvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -16,5 +18,19 @@ public class HelloWorld {
     public String hello(){
         System.out.println("Hello World of Spring MVC");
         return "Hello World of Spring MVC";
+    }
+
+    @ResponseBody
+    @RequestMapping(path = "/test_method", method = RequestMethod.POST)
+    public String testPostMethod() {
+        System.out.println("test post method");
+        return "testPostMethod";
+    }
+
+    @ResponseBody
+    @RequestMapping(path = "/test_method", method = RequestMethod.GET)
+    public String testGetMethod() {
+        System.out.println("test get method");
+        return "testGetMethod";
     }
 }
