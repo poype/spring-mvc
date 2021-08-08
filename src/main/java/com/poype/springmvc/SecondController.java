85734ef9a068405bb96096b470a36f5fe4b327a1
Poype <253186926@qq.com>
2021-08-08 15:33:29 +0800

    * 使用 @RequestParam 将 URI 中的 param 映射到方法中的参数 * 默认 @RequestParam 要求参数必须传递，否则抛 MissingServletRequestParameterException * 可以通过 required 属性将参数设置为可选类型的。这里的 age 参数就是可选的，可以不传，不传时 age 参数为null * 注意这里的 age 参数要用 Integer 类型，不能用 int 类型

--- src/main/java/com/poype/springmvc/SecondController.java
+++ src/main/java/com/poype/springmvc/SecondController.java
@@ -3,6 +3,7 @@ package com.poype.springmvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -17,5 +18,20 @@ public class SecondController {
         System.out.println("id: " + id);
         System.out.println("name: " + name);
         return "testPathVariable";
+    }
+
+    /**
+     * 使用 @RequestParam 将 URI 中的 param 映射到方法中的参数
+     * 默认 @RequestParam 要求参数必须传递，否则抛 MissingServletRequestParameterException
+     * 可以通过 required 属性将参数设置为可选类型的。这里的 age 参数就是可选的，可以不传，不传时 age 参数为null
+     * 注意这里的 age 参数要用 Integer 类型，不能用 int 类型
+     */
+    @ResponseBody
+    @RequestMapping("/test_request_param")
+    public String testRequestParam(@RequestParam("username") String username,
+                                   @RequestParam(value = "age", required = false) Integer age) {
+        System.out.println("username: " + username);
+        System.out.println("age: " + age);
+        return "testRequestParam";
     }
 }
