41145ae2e87390dec890968a9af4c27c437ec84c
Poype <253186926@qq.com>
2021-08-08 22:00:25 +0800

    dataBinder validation

---
+++ src/main/java/com/poype/springmvc/FourthController.java
@@ -0,0 +1,24 @@
+package com.poype.springmvc;
+
+import com.poype.springmvc.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.Valid;
+
+@Controller
+public class FourthController {
+
+    @ResponseBody
+    @RequestMapping("/testBind1")
+    public String testBind1(@Valid User user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            System.out.println("binding fail");
+        } else {
+            System.out.println("user: " + user);
+        }
+        return "testBind1";
+    }
+}
--- src/main/java/com/poype/springmvc/model/User.java
+++ src/main/java/com/poype/springmvc/model/User.java
@@ -1,7 +1,11 @@
 package com.poype.springmvc.model;
 
+
+import javax.validation.constraints.NotEmpty;
+
 public class User {
 
+    @NotEmpty(message="用户名不能为空")
     private String username;
 
     private int age;
